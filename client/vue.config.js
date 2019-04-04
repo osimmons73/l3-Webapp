6973ee474cf85a6376f9bce331e855eb43e08197
Gus <jenkins6g@gmail.com>
2019-04-04 19:34:32 -0400

    proxy from front end to back end server setup

---
+++ client/vue.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  devServer: {
+    proxy: {
+      "/auth/google": {
+        target: "http://localhost:5000",
+        ws: true,
+        changeOrigin: true
+      },
+      "/api/*": {
+        target: "http://localhost:5000"
+      }
+    }
+  }
+};
